cde8270286946cae8fa91793453ae7fff6403cd5
unknown <saisantosh.k@KM-WS53.kmipl.in>
2018-03-07 19:10:00 +0530

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,private iab: InAppBrowser) {
+    const browser = this.iab.create('https://myklovr.com/','_self',{location:'no'}); 
+
+  }
+
+}
